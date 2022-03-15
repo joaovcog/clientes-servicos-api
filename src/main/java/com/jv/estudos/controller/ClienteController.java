a77288f24e8d35b0b9f55c1d98e2d713a6f07d00
Joao Victor <joaovictor.cogomes@gmail.com>
2022-03-15 18:33:57 -0300

    Mapeando metodo para deletar um cliente

--- src/main/java/com/jv/estudos/controller/ClienteController.java
+++ src/main/java/com/jv/estudos/controller/ClienteController.java
@@ -1,10 +1,13 @@
 package com.jv.estudos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,5 +33,19 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente buscarPorId(@PathVariable Integer id) {
 		return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Integer id) {
+		try {
+			clienteRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+//		clienteRepository.findById(id).map(cliente -> {
+//			clienteRepository.delete(cliente);
+//			return Void.TYPE;
+//		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 }
