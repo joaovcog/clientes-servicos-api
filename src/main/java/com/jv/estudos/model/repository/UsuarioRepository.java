ff582bda9dd70d17892fa1f0ad224eb6609b702a
Joao Victor <joaovictor.cogomes@gmail.com>
2022-03-18 18:59:23 -0300

    Configuracao da seguranca da aplicacao

--- src/main/java/com/jv/estudos/model/repository/UsuarioRepository.java
+++ src/main/java/com/jv/estudos/model/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.jv.estudos.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jv.estudos.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	
+	Optional<Usuario> findByUsuario(String usuario);
+	
 }
---
+++ src/main/java/com/jv/estudos/security/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.jv.estudos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jv.estudos.model.entity.Usuario;
+import com.jv.estudos.model.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.findByUsuario(username)
+					.orElseThrow(() -> new UsernameNotFoundException("Usuário inválido."));
+		return User
+				.builder()
+				.username(usuario.getUsuario())
+				.password(usuario.getSenha())
+				.roles("USER")
+				.build();
+	}
+
+}
